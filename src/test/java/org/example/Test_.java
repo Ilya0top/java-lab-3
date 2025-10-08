ae6a0a685fde18f846bb186504aacc171cdfaa87
Ilya0top <ilabezuglyj947@gmail.com>
2025-10-08 15:33:17 +0300

    Add complete Java project: source code, Gradle config, build files

---
+++ src/test/java/org/example/Test_.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test_ {
+
+    @Test
+    void testEmptyList() {
+        assertEquals(15, 10+5);
+    }
+}
